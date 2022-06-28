3bbd33feff82ec5f09cd07da7a12e887ae251aa2
Fray <kozar20000@mail.ru>
2022-06-28 17:49:06 +0300

    even

---
+++ Even/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите  число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a % 2 == 0)
+    Console.WriteLine("{0} четное ", a );
+else
+    Console.WriteLine("{0} нечетное ", a );
+
+    
+    
+
+
+
